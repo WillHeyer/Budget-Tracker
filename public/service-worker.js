619f76e0c942f6e51a4deaecb3bc0b788a210734
Will Heyer <williamhheyer@gmail.com>
2021-03-02 23:35:06 -0600

    Adding updated Files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,71 @@
+
 const FILES_TO_CACHE = [
-  '/db.js',
-  '/index.html',
-  '/index.js',
-  '/manifest.webmanifest',
-  '/icons/icon-192x192.png',
-  '/styles.css'
-];
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/style.css"];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+  const CACHE_NAME = 'static-cache-v13';
+  const DATA_CACHE_NAME = 'data-cache-v8';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
+  //Install service worker
+  self.addEventListener('install', evt => {
+      evt.waitUntil(
+          caches.open(CACHE_NAME).then(cache =>{
+              console.log('Your files were pre-cached successfully');
+              return cache.addAll(FILES_TO_CACHE);
           })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+      );
+      self.skipWaiting();
+  });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  // Activate Service Worker
+  self.addEventListener('activate', evt => {
+      evt.waitUntil(
+          caches.keys().then(keyList => {
+              return Promise.all(
+                  keyList.map( key => {
+                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                          console.log('Removing old cache data', key);
+                          return caches.delete(key);
+                      }
+                  })
+              );
+          })
+      );
+      self.clients.claim();
+  });
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+  // 5. Fetch Files
+  self.addEventListener('fetch',  evt =>{
+      if (evt.request.url.includes('/api/')) {
+          console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+          evt.respondWith(
+              caches.open(DATA_CACHE_NAME).then(cache => {
+                  return fetch(evt.request)
+                      .then(response  => {
+                          if (response.status === 200) {
+                              cache.put(evt.request.url, response.clone());
+                          }
+                          return response;
+                      })
+                      .catch(err => {
+                          return cache.match(evt.request);
+                      });
+              })
+          );
+
+          return;
+      }
+
+      evt.respondWith(
+          caches.open(CACHE_NAME).then( cache => {
+              return cache.match(evt.request).then(response => {
+                  return response || fetch(evt.request)
+              });
+
+          })
+      );
+  });
